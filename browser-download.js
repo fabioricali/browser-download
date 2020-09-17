79c9667afe6fbc8917306b0d9f969e04cd490b9c
Fabio Ricali <f.ricali@xdevel.com>
2020-09-17 18:15:34 +0200

    add files to callback

--- browser-download.js
+++ browser-download.js
@@ -7,20 +7,22 @@ class BrowserDownload {
     async run(files) {
         try {
             this.isRunning = true;
+            let totalFilesComplete = 0;
             if (typeof this.opts.onStart === 'function')
                 this.opts.onStart(this);
             if (files.length > 1) {
                 let zip = new JSZip();
                 for (let i = 0; i < files.length; i++) {
                     if (typeof this.opts.onDownloadFileStart === 'function')
-                        this.opts.onDownloadFileStart(files[i], files, this);
+                        this.opts.onDownloadFileStart(files[i], files, totalFilesComplete, this);
 
                     let response = await fetch(files[i].url);
                     let blob = await response.blob();
                     zip.file(files[i].name, blob);
 
+                    totalFilesComplete++;
                     if (typeof this.opts.onDownloadFileEnd === 'function')
-                        this.opts.onDownloadFileEnd(files[i], files, this);
+                        this.opts.onDownloadFileEnd(files[i], files, totalFilesComplete, this);
                 }
 
                 if (typeof this.opts.onZipStart === 'function')
@@ -34,12 +36,15 @@ class BrowserDownload {
                     this.opts.onZipEnd(this);
             } else {
                 if (typeof this.opts.onDownloadFileStart === 'function')
-                    this.opts.onDownloadFileStart(files[0], this);
+                    this.opts.onDownloadFileStart(files[0], files, totalFilesComplete, this);
 
-                saveAs(files[0].url, files[0].name);
+                let response = await fetch(files[0].url);
+                let blob = await response.blob();
+                saveAs(blob, files[0].name);
 
+                totalFilesComplete++;
                 if (typeof this.opts.onDownloadFileEnd === 'function')
-                    this.opts.onDownloadFileEnd(files[0], this);
+                    this.opts.onDownloadFileEnd(files[0], files, totalFilesComplete, this);
             }
             if (typeof this.opts.onEnd === 'function')
                 this.opts.onEnd(null, this);
